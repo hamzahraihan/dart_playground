38c313f4dcc315b636b7b435965db7d815e463b4
hamzah raihan <101212035+HamzahRaihan@users.noreply.github.com>
2024-10-05 15:24:15 +0700

    Add functions

--- bin/dart_playground.dart
+++ bin/dart_playground.dart
@@ -27,10 +27,41 @@
 // }
 
 void main() {
+  // return string value
   greet('hamzah', 2002);
+
+  // return number value
+  int firstNumber = 7;
+  int secondNumber = 10;
+  print(
+      'Rata-rata dari $firstNumber & $secondNumber adalah ${average(firstNumber, secondNumber)}');
+  greetNewUser(name: 'windy', age: 20, isVerified: true);
 }
 
 void greet(String name, int bornYear) {
   var age = 2024 - bornYear;
   print('halo $name!, Tahun ini anda berusia $age tahun');
 }
+
+double average(int firstNumber, int secondNumber) =>
+    (firstNumber + secondNumber) / 2;
+
+// * Optional Parameters
+/* 
+* Dart mendukung optional parameter, di mana kita tidak wajib mengisi parameter yang diminta oleh fungsi. Untuk bisa membuat parameter menjadi opsional, kita perlu memasukkannya ke dalam kurung siku. Setiap parameter yang tidak dimasukkan akan memiliki nilai null. Namun sejak versi 2.12, Dart memiliki fitur null safety di mana suatu variabel secara default tidak bisa memiliki nilai null. Solusinya, kita bisa menambahkan tanda tanya (?) setelah tipe data supaya bisa menyimpan nilai null atau memberikan nilai parameter default. Nilai ini akan digunakan jika kita tidak memasukkan parameternya.
+*/
+
+// void greetNewUser([String? name, int? age, bool isVerified = false]) =>
+//     print('halo $name, $age, $isVerified');
+
+// * dengan cara diatas rutan parameter masih perlu diperhatikan sehingga jika kita hanya ingin mengisi parameter terakhir, kita perlu mengisi parameter sebelumnya dengan null.
+
+// * Untuk mengatasi masalah di atas kita bisa memanfaatkan named optional parameters. Pada opsi ini kita menggunakan kurung kurawal pada parameter.
+// void greetNewUser({String? name, int? age, bool isVerified = false}) =>
+//     print('halo $name, $age, $isVerified');
+
+/*
+* Perhatikan bahwa parameter ini bersifat opsional dan secara default akan bernilai null. Untuk memenuhi null safety, Anda bisa menggunakan cara seperti sebelumnya, atau menandai parameter wajib diisi dengan keyword required.
+*/
+void greetNewUser({required String name, int? age, bool isVerified = false}) =>
+    print('halo $name, $age, $isVerified');
